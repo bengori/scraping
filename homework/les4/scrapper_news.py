2a9fd86f3d643ac2cff916e86e200f124da9ba6f
nataly.akhaimova <bengori22@gmail.com>
2021-02-18 22:39:33 +0500

    hw4 scrapper update: add yandex_news

--- homework/les4/scrapper_news.py
+++ homework/les4/scrapper_news.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import requests
 from lxml import html
+import datetime
 
 my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                             'Chrome/88.0.4324.150 Safari/537.36'}
@@ -32,6 +33,37 @@ def requests_lenta_news():
     return base_news
 
 
+def requests_yandex_news():
+    url = 'https://yandex.ru/news'
+    response = requests.get(f'{url}', headers=my_headers)
+    root = html.fromstring(response.text)
+    news_block = root.xpath("//div[contains(@class, 'news-app__top')][last()-1]//article")
+    base_news = []
+    for item in news_block:
+        news = {}
+        name_source = item.xpath(".//span[contains(@class, 'mg-card-source__source')]//text()")
+        name_news = item.xpath(".//div[contains(@class, 'mg-card__text')]//h2/text()")
+        link_news = item.xpath(".//div[contains(@class, 'mg-card__text')]/a/@href")
+        date_news_info = item.xpath(".//span[contains(@class, 'mg-card-source__time')]/text()")
+
+        try:
+            yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%d.%m.%Y')
+            date_news_info = date_news_info.replace('Вчера', yesterday)
+        except AttributeError:
+            today = datetime.date.today().strftime('%d.%m.%Y')
+            date_news_info = f'{today} {date_news_info[0]}'
+
+        news['name_source'] = name_source[0]
+        news['name_news'] = name_news[0]
+        news['link_news'] = link_news[0]
+        news['date_news'] = date_news_info
+        base_news.append(news)
+
+    return base_news
+
+
 if __name__ == "__main__":
     lenta_news = requests_lenta_news()
     pprint(lenta_news)
+    yandex_news = requests_yandex_news()
+    pprint(yandex_news)

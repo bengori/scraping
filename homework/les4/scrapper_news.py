80b78d1968bcb4a3412603e42447f5dc8f0683fd
nataly.akhaimova <bengori22@gmail.com>
2021-02-23 15:52:27 +0500

    hw4 update: учла замечания

--- homework/les4/scrapper_news.py
+++ homework/les4/scrapper_news.py
@@ -25,7 +25,6 @@ def requests_lenta_news():
         date_news_info = root.xpath(".//div[@class = 'b-topic__info']/time/@datetime")
         date_news = date_news_info[0][:10].split('-')
         date_news = datetime.date(int(date_news[0]), int(date_news[1]), int(date_news[2])).strftime('%d.%m.%Y')
-        # либо date_news = datetime.date(int(date_news[0]), int(date_news[1]), int(date_news[2]))
         news['name_source'] = name_source
         news['name_news'] = name_news[0].replace(u'\xa0', ' ')
         news['link_news'] = link_news
@@ -40,7 +39,8 @@ def requests_yandex_news():
     url = 'https://yandex.ru/news'
     response = requests.get(f'{url}', headers=my_headers)
     root = html.fromstring(response.text)
-    news_block = root.xpath("//div[contains(@class, 'news-app__top')][last()-1]//article")
+    # замечания по работе: лучше изнутри наружу, так как порядок блоков новостей может изменится
+    news_block = root.xpath("//article[contains(@class,'mg-card')]//a[contains(@href,'rubric=science') and @class='mg-card__link']/ancestor::article")
     base_news = []
     for item in news_block:
         news = {}
@@ -64,30 +64,27 @@ def requests_yandex_news():
 
     return base_news
 
-
+# оптимизировать можно: на главной опросить ссылки, всю остальную информацию собрать на странице новости
 def requests_mail_news():
     url = 'https://news.mail.ru/'
     response = requests.get(f'{url}', headers=my_headers)
     root = html.fromstring(response.text)
-    news_block = root.xpath("//td//div[contains(@class, 'daynews__item')]|//ul[contains(@class, 'list list_type_square list_half js-module')]/li[position()>2]")
-
+    news_block= root.xpath("//div[contains(@class,'daynews__')]/a | //ul[@data-module='TrackBlocks']/li[@class='list__item']/a")
     base_news = []
 
     for i, item in enumerate(news_block, 1):
         news = {}
         if i < 6:
-            name_news = item.xpath(".//span[contains(@class, 'js-topnews__notification')]/text()")
+            name_news = item.xpath(".//span[contains(@class, 'photo__title')]/text()")
         else:
-            name_news = item.xpath(".//a/text()")
-        link_news = item.xpath(".//a/@href")
+            name_news = item.xpath(".//text()")
+        link_news = item.xpath(".//@href")
         response_2 = requests.get(link_news[0], headers=my_headers)
         dom = html.fromstring(response_2.text)
         name_source = dom.xpath("//span[contains(@class, 'breadcrumbs__item')]//a//text()")
-        date_news_info = dom.xpath(
-            "//span[contains(@class, 'breadcrumbs__item')]//span[contains(@class, 'js-ago')]/@datetime")
+        date_news_info = dom.xpath("//span[contains(@class, 'breadcrumbs__item')]//span[contains(@class, 'note__text')]/@datetime")
         date_news = date_news_info[0][:10].split('-')
         date_news = datetime.date(int(date_news[0]), int(date_news[1]), int(date_news[2])).strftime('%d.%m.%Y')
-        # либо date_news = datetime.date(int(date_news[0]), int(date_news[1]), int(date_news[2]))
         news['name_source'] = name_source[0]
         news['name_news'] = name_news[0].replace(u'\xa0', ' ')
         news['link_news'] = link_news[0]
